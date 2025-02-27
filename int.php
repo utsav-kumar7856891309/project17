1c1c8c2489925a6f68de64ee99da62940e0c6cd0
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-02-27 22:21:08 +0530

    password validation

--- int.php
+++ int.php
@@ -3,46 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Number cheker</title>
+    <title>Basic login pages</title>
 </head>
 <body>
-    <h2>Number checker</h2>
+    <h2>Login pages</h2>
     <form method="POST" action="">
-        <label for="number">Enter a number:</label>
-        <input type="number" id="number" name="number" required>
-        <button type="submit">Check</button>
+        <label for="username">USername:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Login</button>
+
+        
     </form>
     <h3>Result:</h3>
     <?php 
-     function isprime($num){
-        if($num<=1){
-            return false;
-        }
-        for($i=2;$i<=sqrt($num);$i++){
-            if($num%$i==0){
-                return false;
-            }
-        }
-        return true;
-     }
+     $correctUsername="admin";
+     $correctPassword="password123";
      if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $number=intval($_POST['number']);
-        if($number==0){
-            echo "o is neither even odd ,prime nor compostie";
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        if($username===$correctUsername && $password===$correctPassword){
+            echo "Login successful! Welcome, $username.";
         }else{
-            if($number%2==0){
-                echo "$number is Even.<br>";
-            }else{
-                echo "$number is odd.<br>";
-            }
-            if(isprime($number)){
-                echo "$number is a prime number.";
-            }else{
-                echo "$number is a compostie number.";
-            }
+            echo "Invalid username or password ,Plesae try again.";
         }
      }
-    
     
     
     ?>
